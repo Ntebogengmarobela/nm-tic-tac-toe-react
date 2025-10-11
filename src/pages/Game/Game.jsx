39574e9c53c6dcdc5f6c086ce95ecc23de8832dc
Ntebogeng Marobela <officialntebo@gmail.com>
2025-10-11 02:28:10 +0200

    Commented out unused 'board' variable to fix Netlify build

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -6,7 +6,7 @@ import { GameContext } from '../../contexts/GameContext'
 
 const Game = () => {
   const {game} = useContext(GameContext);
-  const board= [1,2,3,4,5,6,7,8,9];
+  // const board= [1,2,3,4,5,6,7,8,9];
   return (
     <Container>
       <GameBoardStyle>
